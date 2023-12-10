9a64c11c4c9c9746bf0f96047289e2d837b698d3
redwardo9 <reh425@nyu.edu>
2023-12-09 19:27:37 -0500

    added comments!

--- ledBlink.ino
+++ ledBlink.ino
@@ -2,15 +2,20 @@ int ledPin = 13;
 int ledPin2 = 14;
 
 void setup() {
+  //set up leds
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
+  //turn leds on
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
+  //wait 500 ms
   delay(500);
+  //turn leds off
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
+  //wait 500ms
   delay(500);
 }
