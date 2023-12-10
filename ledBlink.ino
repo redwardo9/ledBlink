946a04a0c9cdc45cd602ea3bedf4ef936bc1608a
redwardo9 <reh425@nyu.edu>
2023-12-09 19:26:26 -0500

    leds blink

--- ledBlink.ino
+++ ledBlink.ino
@@ -2,12 +2,15 @@ int ledPin = 13;
 int ledPin2 = 14;
 
 void setup() {
-pinMode(ledPin, OUTPUT);
-pinMode(ledPin2, OUTPUT);
-
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(500);
 }
